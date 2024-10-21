46923eeddde64f166cc32101f577448c929246bc
Guillaume Infantes <guillaume.infantes@jolibrain.com>
2024-10-21 16:18:59 +0000

    refacto

--- psp/models/gnn_mp.py
+++ psp/models/gnn_mp.py
@@ -34,6 +34,7 @@ from .agent_observation import AgentObservation
 from .agent_graph_observation import AgentGraphObservation
 from .edge_embedder import PspEdgeEmbedder
 from .rewiring import rewire, homogeneous_edges
+from .gnn_flat import GnnFlat
 
 
 class GnnMP(torch.nn.Module):
@@ -274,6 +275,24 @@ class GnnMP(torch.nn.Module):
                     )
                 )
 
+        self.gnn_flat = GnnFlat(
+            self.input_dim_features_extractor,
+            hidden_dim_features_extractor,
+            n_layers_features_extractor,
+            n_mlp_layers_features_extractor,
+            layer_pooling,
+            n_attention_heads,
+            normalize,
+            activation_features_extractor,
+            residual,
+            rwpe_k,
+            self.rwpe_h,
+            update_edge_features,
+            update_edge_features_pe,
+            shared_conv,
+            pyg,
+        )
+
     def reset_egat(self):
         for egat in self.features_extractors:
             egat.reset_parameters()
@@ -371,6 +390,8 @@ class GnnMP(torch.nn.Module):
                 ),
             )
             pe = g.ndata["pe"]
+        else:
+            pe = None
 
         g = g.to(next(self.parameters()).device)
         edge_features = self.edge_embedder(g)
@@ -403,116 +424,7 @@ class GnnMP(torch.nn.Module):
                 res_cal_id.to(features.device)
             )
 
-        if self.normalize:
-            features = self.norm0(features)
-
-        if self.layer_pooling == "all":
-            features_list = []
-            edge_scores_list = []
-            if self.rwpe_k != 0:
-                features_list.append(torch.cat([features, pe], dim=-1))
-            else:
-                features_list.append(features)
-            if self.update_edge_features:
-                edge_scores_list.append(edge_scores)
-
-        features = self.features_embedder(features)
-
-        if self.normalize:
-            features = self.norm1(features)
-
-        if self.layer_pooling == "all":
-            if self.rwpe_k != 0:
-                features_list.append(torch.cat([features, pe], dim=-1))
-            else:
-                features_list.append(features)
-
-        if self.layer_pooling == "last":
-            previous_feat = features
-            if self.update_edge_features:
-                previous_edge_scores = edge_scores
-            if self.rwpe_k != 0:
-                previous_pe = pe
-
-        for layer in range(self.n_layers_features_extractor):
-            if self.rwpe_k != 0:
-                features = torch.cat([features, pe], dim=-1)
-
-            if self.update_edge_features:
-                features, _, new_e_attr = self.features_extractors[layer](
-                    g, features, edge_features, efeats_e=edge_scores
-                )
-                edge_scores = self.mlps_edges[layer](
-                    new_e_attr.flatten(start_dim=-2, end_dim=-1)
-                )
-                features = self.mlps[layer](features.flatten(start_dim=-2, end_dim=-1))
-
-            else:
-                if self.shared_conv:
-                    features, _ = self.features_extractors(
-                        g._graph,
-                        features,
-                        edge_features,
-                    )
-                    features = self.mlps(features)
-                else:
-                    features, _ = self.features_extractors[layer](
-                        g._graph,
-                        features,
-                        edge_features,
-                    )
-                    features = self.mlps[layer](features)
-
-            if self.rwpe_k != 0:
-                if self.update_edge_features_pe:
-                    pe, new_e_pe_attr = self.pe_conv[layer](
-                        g._graph, pe, edge_features_pe.clone()
-                    )
-                    edge_features_pe += self.mlps_edges_pe[layer](
-                        new_e_pe_attr.flatten(start_dim=-2, end_dim=-1)
-                    )
-                else:
-                    pe, _ = self.pe_conv[layer](g._graph, pe, edge_features_pe)
-                pe = self.pe_mlp[layer](pe)
-
-            # if self.layer_pooling == "all":
-            #     features_list.append(features)
-            if self.normalize:
-                features = self.norms[layer](features)
-
-            if self.residual:
-                if self.layer_pooling == "all":
-                    features += features_list[-1][:, : self.hidden_dim]
-                    if self.update_edge_features:
-                        edge_scores += edge_scores_list[-1]
-                    if self.rwpe_k != 0:
-                        pe += features_list[-1][:, self.hidden_dim :]
-                else:
-                    features += previous_feat[:, : self.hidden_dim]
-                    if self.update_edge_features:
-                        edge_scores += previous_edge_scores
-                        previous_edge_scores = edge_scores
-                    if self.rwpe_k != 0:
-                        pe += previous_pe
-                if self.normalize:
-                    features = self.normsbis[layer](features)
-                if self.layer_pooling == "last":
-                    previous_feat = features
-                    if self.rwpe_k != 0:
-                        previous_pe = pe
-
-            if self.layer_pooling == "all":
-                if self.rwpe_k != 0:
-                    features_list.append(torch.cat([features, pe], dim=-1))
-                else:
-                    features_list.append(features)
-                if self.update_edge_features:
-                    edge_scores_list.append(edge_scores)
-
-        if self.layer_pooling == "all":
-            features = torch.cat(
-                features_list, axis=1
-            )  # The final embedding is concatenation of all layers embeddings
+        features = self.gnn_flat(g, features, edge_features, pe)
 
         node_features = features[:num_nodes, :]
 
